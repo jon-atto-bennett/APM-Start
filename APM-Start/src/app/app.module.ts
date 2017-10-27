14cc7ac1616623592ba7b3bde025699706ae0dd6
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-10-27 15:16:38 +1300

    nested rating component

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component'
+import { ProductListComponent } from './products/product-list.component';
+import { StarComponent } from './shared/star.component'
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
+    StarComponent,
     ConvertToSpacesPipe
   ],
   imports: [
